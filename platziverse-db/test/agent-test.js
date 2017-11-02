e747cdf496e0fa1320027c3472708ac5ad34b656
Jonathan <jalcantara1990@gmail.com>
2017-11-02 18:36:53 +0100

    agent test with stub and mocks defined

--- platziverse-db/test/agent-test.js
+++ platziverse-db/test/agent-test.js
@@ -1,20 +1,64 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+const agentFixtures = require('./fixtures/agent')
 
 let config = {
   logging: function () {}
 }
 
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let single = Object.assign({}, agentFixtures.single)
+let id = 1
+let AgentStub = null
 let db = null
+let sandbox = null
 // test('make it pass', t => {
 //   t.pass()
 // })
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.sandbox.create()
+
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  // Model findById Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.sandbox.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent Service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the AgentModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+  t.true(AgentStub.findById.called, 'findById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with specifies ID')
+  t.deepEqual(agent, agentFixtures.byId(id), 'Should be the same')
 })
