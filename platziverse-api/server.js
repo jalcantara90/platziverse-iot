8e7c8df6156aeb140be1a64d1f3d851bd9edacb6
Jonathan <jalcantara1990@gmail.com>
2017-11-05 15:14:41 +0100

    handle error defined

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const http = require('http')
+const debug = require('debug')('Platziverse:api-server')
 const chalk = require('chalk')
 const express = require('express')
 
@@ -11,6 +12,26 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/api', api)
+
+// Express errorHandler
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/nor found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
+})
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncauhtExeption', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
   console.log(`${chalk.green('--> [Platziverse-API]')} server listening on port ${port}`)
